727027ea90d1a5b68214b26e0dd82323fb316d26
DSTOLF <dstolf@gmail.com>
2022-01-25 07:06:13 -0500

    removed mssql snapins requirements, added an rman connection before restore, to build dynamic rman views and remove sbt backups from ctrl file

--- src/resources/ds_crtRestoreScripts.ps1
+++ src/resources/ds_crtRestoreScripts.ps1
@@ -63,7 +63,21 @@ $PSDefaultParameterValues['*:Encoding'] = 'ascii'
 
  #### there are two reasons for connecting to RMAN
  #### 1) v$rman views might not be present in a mounted database unless you first connect to it with RMAN
+
  $testRman ='exit;' 
+
+ $result = $testRman | . $Env:ORACLE_HOME\bin\rman.exe target /
+
+ #### 2) the control file might have some SBT backups in its catalog, which will cause error during restore
+ $testRman =@"
+ allocate channel for maintenance device type sbt parms 'SBT_LIBRARY=oracle.disksbt, ENV=(BACKUP_DIR=c:\tmp)';
+ delete noprompt force obsolete device type SBT;;
+ crosscheck backup;
+ delete nonprompt backup device type SBT;
+ crosscheck backup;
+ delete force noprompt expired backup device type SBT;;
+ exit
+"@ 
 
  $result = $testRman | . $Env:ORACLE_HOME\bin\rman.exe target /
 
@@ -132,21 +146,6 @@ mv $stgMnt\$oraSrc\new_ctl_bkp_endscn.txt $stgMnt\$oraSrc\last_ctl_bkp_endscn.tx
 echo $end_scn > "$stgMnt\$oraSrc\new_ctl_bkp_endscn.txt"
 
 remove_empty_lines "$stgMnt\$oraSrc\new_ctl_bkp_endscn.txt"
-
- #### there are two reasons for connecting to RMAN
- #### 1) v$rman views might not be present in a mounted database unless you first connect to it with RMAN
- #### 2) the control file might have some SBT backups in its catalog, which will cause error during restore
- $testRman =@"
- allocate channel for maintenance device type sbt parms 'SBT_LIBRARY=oracle.disksbt, ENV=(BACKUP_DIR=c:\tmp)';
- delete noprompt force obsolete;
- crosscheck backup;
- delete nonprompt backup device type SBT;
- crosscheck backup;
- delete force noprompt expired backup;
- exit
-"@ 
-
- $result = $testRman | . $Env:ORACLE_HOME\bin\rman.exe target /
 
 #### Create RMAN restore script
 
--- src/resources/ds_inc_crtRestoreScripts.ps1
+++ src/resources/ds_inc_crtRestoreScripts.ps1
@@ -61,7 +61,21 @@ $PSDefaultParameterValues['*:Encoding'] = 'ascii'
 
  #### there are two reasons for connecting to RMAN
  #### 1) v$rman views might not be present in a mounted database unless you first connect to it with RMAN
- $testRman ='exit;'
+
+ $testRman ='exit;' 
+
+ $result = $testRman | . $Env:ORACLE_HOME\bin\rman.exe target /
+
+ #### 2) the control file might have some SBT backups in its catalog, which will cause error during restore
+ $testRman =@"
+ allocate channel for maintenance device type sbt parms 'SBT_LIBRARY=oracle.disksbt, ENV=(BACKUP_DIR=c:\tmp)';
+ delete noprompt force obsolete device type SBT;;
+ crosscheck backup;
+ delete nonprompt backup device type SBT;
+ crosscheck backup;
+ delete force noprompt expired backup device type SBT;;
+ exit
+"@ 
 
  $result = $testRman | . $Env:ORACLE_HOME\bin\rman.exe target /
 
@@ -179,21 +193,6 @@ $diff = Compare-Object $postdf $predf | ?{$_.sideindicator -eq '<='} | Select-Ob
 log "New Datafiles, $diff"
 
 log "Compare Pre and Post Datafiles, $oraUnq FINISHED"
-
- #### there are two reasons for connecting to RMAN
- #### 1) v$rman views might not be present in a mounted database unless you first connect to it with RMAN
- #### 2) the control file might have some SBT backups in its catalog, which will cause error during restore
- $testRman =@"
- allocate channel for maintenance device type sbt parms 'SBT_LIBRARY=oracle.disksbt, ENV=(BACKUP_DIR=c:\tmp)';
- delete noprompt force obsolete;
- crosscheck backup;
- delete nonprompt backup device type SBT;
- crosscheck backup;
- delete force noprompt expired backup;
- exit
-"@ 
-
- $result = $testRman | . $Env:ORACLE_HOME\bin\rman.exe target /
 
 #### Create RMAN restore script
 
